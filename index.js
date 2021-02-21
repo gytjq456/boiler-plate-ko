792e2865e2becc367b5ef6fa801380aa8e29bdc7
LEE HYO SUB <gytjq456@naver.com>
2021-02-21 21:33:55 +0900

    login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const {User} = require("./models/User");
 
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 // application/json 파일을 분석해서 가져오는
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 const mongoose = require('mongoose')
@@ -46,6 +48,46 @@ app.post('/register',(req, res) => {
   })
 })
 
+app.post('/login',(req,res) => {
+  // 데이터베이스에서 존재여부 찾기 
+  User.findOne({email : req.body.email}, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSucess : false,
+        message : "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 비밀번호 일치 여부
+    user.comparePassword(req.body.password, (err, isMatch) =>{
+      if(!isMatch){
+        // 같지않음
+        return res.json({
+          loginSucess : false,
+          message : "비밀번호가 틀렸습니다."
+        })
+      }
+        // 비밀번호 까지 맞다면 토큰을 생성하기
+        user.generateToken((err, user) =>{
+          if(err) {
+            return res.status(400).send(err);
+          }
+          
+          // 토큰을 저장한다. 어디에?? 쿠키? 로컬스토리지
+          res.cookie("x_auth", user.token).status(200).json({
+            loginSucess : true,
+            userId : user._id
+          })
+        })      
+    })
+
+  })
+
+
+  
+
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const saltRounds = 10; // 몇글자인지 
+const saltRounds = 5; // 몇글자인지 
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name : {
@@ -15,7 +16,7 @@ const userSchema = mongoose.Schema({
   },
   password : {
     type: String,
-    maxlength : 50
+    maxlength : 70
   },
   lastName : {
     type : String,
@@ -51,7 +52,7 @@ userSchema.pre('save',function(next){
         // Store hash in your password DB.
           //myPlaintextPassword(1번째 인자) = 순수 비번
           //hash == 암호화된 비번
-          console.log(salt)
+          console.log(hash)
           if(err){
             next(err)
           }
@@ -59,8 +60,40 @@ userSchema.pre('save',function(next){
           next();
       });
     });
+  }else{
+    // 다른내용을 바꾸는 경우
+    next();
   }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+ // plainPassword : 암호화되지 않는 비번
+ bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+  if(err){
+    // 같지 않음
+    return cb(err)
+  }
+  cb(null, isMatch)
+ })
+}
+
+userSchema.methods.generateToken = function(cb){
+  var user = this;
+
+  // jsonwebtoken을 이용해서 토큰 생성
+  // _id = db Id
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  // user_id + 'secretToken = token;
+  user.token = token;
+  user.save(function(err, user){
+    if(err){
+      return cb(err)
+    }
+    cb(null, user)
+  })
+}
+
+
 
 const User = mongoose.model('User',userSchema);
 
